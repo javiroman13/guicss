ef323aee60f602943690e7055a371d8e13b1d8d5
javier roman <22670018@itiguala.edu.mx>
2023-05-30 14:31:15 -0600

    guicss

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.guicss {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.guicss to javafx.fxml;
     exports com.example.guicss;
 }
